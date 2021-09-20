f14dc54f95db4965ea88acac97d53c21d9069582
andreewaD <51123966+andreewD@users.noreply.github.com>
2021-09-20 10:48:32 -0500

    Dir fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Content from "./components/Content/Content";
 import Navigation from "./components/Nav-Bar/Navigation";
-
+import Cotizacion from "./components/pages/Cotizacion/Cotizacion";
 
 function App() {
   return (
     <div >
       <Navigation />
-      <Content />
+      {/* <Content /> */}
+      <Cotizacion/>
     </div>
   );
 }
