5e56f88c92d06684a12f185235a083040d2db250
Edwin <eyauyos@uni.pe>
2021-09-21 12:26:07 -0500

    Benefits component 50%

--- src/components/Home/Benefits/Benefits.js
+++ src/components/Home/Benefits/Benefits.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import image from '../../../assets/imgs/Benefits/img.png'
+import './Benefit.scss'
 
-export default function Benefits() {
+
+export default function Benefits(props) {
+
+    const data = {
+        "t1" : "",
+        "t2" : "",
+        "listData": []
+    }
+
+    const listItems = props.data.listData.map((item) =>
+        <li key={item.toString()}>
+            {item}
+        </li>
+    );
     return (
-        <div>
-            
+        <div className="benefitsContainer">
+            <div className="t1">{props.data.t1}</div>
+            <div className="t2">{props.data.t2}</div>
+            <div className="infoContainer">
+                <div className="imageContainer">
+                    <img className="image" src={image} alt={image}></img>
+                </div>
+
+                <ul className="list">
+                    {listItems}
+                </ul>
+            </div>
+
         </div>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import Content from "../../components/Home/Content/Content";
 import Benefits from "../../components/Home/Benefits/Benefits";
+import HomeData from "./HomeData.json"
+// const dataBenefits = [
+//     "Originalidad de trabajo elaborado.",
+//     "Levantamiento de observaciones al 100%, hasta que se apruebe el trabajo.",
+//     "Especialistas encargados de la elaboración de tu investigación.",
+//     "Confidencialidad total de tus datos.",
+//     "Asesoría de una hora al concluir con todos los entregables suscritos.",
+//     "Cronograma de entregables (semanal) para que usted pueda dar seguimiento.",
+//     "Hasta 2 sesiones de dos horas en la semana para coordinaciones o explicaciones de avance."
+// ]
 
 export default function Home() {
     return (
         <div>
             <Content/>
             {/* FALTA CUADRO */}
-            <Benefits/>
+            <Benefits data={HomeData.benefits}/>
         </div>
     )
 }
