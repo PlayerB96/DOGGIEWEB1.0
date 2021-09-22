e96669668b00e05e84527d84eb6c4529d7c9adcc
Edwin <eyauyos@uni.pe>
2021-09-22 18:26:40 -0500

    Enrutamiento de las vistas que se tienen: Home Services

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Presentation from "../../components/Home/Presentation/Presentation";
 import Benefits from "../../components/Home/Benefits/Benefits";
-import About_us_frame from '../../components/Home/AboutUsFrame/AboutUsFrame';
+import AboutUsFrame from '../../components/Home/AboutUsFrame/AboutUsFrame';
 import Services from '../../components/Home/OurServices/Services';
 import HomeData from "./HomeData.json"
+import FinalSection from '../../components/Home/FinalSection/FinalSection';
+
 // const dataBenefits = [
 //     "Originalidad de trabajo elaborado.",
 //     "Levantamiento de observaciones al 100%, hasta que se apruebe el trabajo.",
@@ -18,9 +20,10 @@ export default function Home() {
     return (
         <div>
             <Presentation data={HomeData.presentation}/>
-            <About_us_frame data={HomeData.about}/>
+            <AboutUsFrame data={HomeData.about}/>
             <Benefits data={HomeData.benefits}/>
             <Services/>
+            <FinalSection data={HomeData.finalSection}/>
         </div>
     )
 }
