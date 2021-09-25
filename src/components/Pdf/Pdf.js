6f75b3d35aa7153a2ecabc7ae47b39d3752b6519
andreewaD <51123966+andreewD@users.noreply.github.com>
2021-09-25 16:57:58 -0500

    pdf body cells complete

--- src/components/Pdf/Pdf.js
+++ src/components/Pdf/Pdf.js
@@ -4,11 +4,12 @@ import WaterMark from "../../assets/imgs/Pdf/marcaAgua.svg"
 import Logo from "../../assets/imgs/Pdf/Logo.svg"
 import './Pdf.scss'
 
-export default function Pdf() {
+export default function Pdf(props) {
 
     return (
         <div className="docBody">
             <div className="border">
+                <div className="bluebar"></div>
                 <img src={BaseImg} alt="" />
                 <img className="logo" src={Logo} alt="" />
                 <img className="waterMark" src={WaterMark} alt="" />
@@ -28,17 +29,25 @@ export default function Pdf() {
                         </div>
 
                     </div>
-                    <div className="bodyCells">
-                        <div className="cotizacion">COTIZACION</div>
-                        <div className="rightCells">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
+                    <div className="firstBodyCells">
+                        <div className="cotizacion">COTIZACION
                         </div>
-                        <div className="rightCells">
-                            <div className="cell"></div>
-                            <div className="cell"></div>
+                        <div className="littleCellsLeft">
+                            <div className="leftSon">a</div>
+                            <div className="leftSon">b</div>
                         </div>
+                        <div className="littleCellsRight">
+                            <div className="rightSon"></div>
+                            <div className="rightSon"></div>
+                        </div>
+                    </div>
 
+                    <div className="secondBodyCells">
+                        <div className="leftSon"></div>
+                        <div className="rightSon"></div>
+                    </div>
+
+                    <div className="thirdBodyCells">
 
                     </div>
                 </div>
