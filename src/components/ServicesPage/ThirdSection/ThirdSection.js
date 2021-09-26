ce78bc11992dc8105ab8f47a4a6ca15d68c4118c
Sergio Yepez <sergio.2435@hotmail.com>
2021-09-26 18:38:16 -0500

    ThirdSection Responsive

--- src/components/ServicesPage/ThirdSection/ThirdSection.js
+++ src/components/ServicesPage/ThirdSection/ThirdSection.js
@@ -5,29 +5,29 @@ import './ThirdSection.scss';
 export default function ThirdSection(props){
     return(
         <div className="Container">
-            <div className="All">
+            <div className="Left">
                 <div className="Container-Description">
-                        <h1 id="Black">{props.data.t1}</h1>
-                        <h2 id="Orange">{props.data.t2}</h2>
-                        <p id="Description">{props.data.description}</p>
-                    <div className="Box-Text">
-                        <div className="Text">
-                            <img className="Point" src={Point} alt="Point"></img>
+                        <h1>{props.data.t1}</h1>
+                        <h2>{props.data.t2}</h2>
+                        <p>{props.data.description}</p>
+                    <div className="Box-Details">
+                        <div className="Details">
+                            <img className="CheckImg" src={Point} alt="Point"></img>
                             <p className="p2">{props.data.point1}</p>
                         </div>
-                        <div className="Text">
-                            <img className="Point" src={Point} alt="Point"></img>
+                        <div className="Details">
+                            <img className="CheckImg" src={Point} alt="Point"></img>
                             <p className="p2">{props.data.point2}</p>
                         </div>
-                        <div className="Text">
-                            <img className="Point" src={Point} alt="Point"></img>
+                        <div className="Details">
+                            <img className="CheckImg" src={Point} alt="Point"></img>
                             <p className="p2">{props.data.point3}</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="Image">
-                <img id="grafico" src={Study} alt="Study"></img>
+            <div className="Right">
+                <img id="Image-Right" src={Study} alt="Study"></img>
             </div>
         </div>
     )
