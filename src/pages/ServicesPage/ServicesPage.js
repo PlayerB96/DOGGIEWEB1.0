0e59dfb6387e45eb4a47c6aaa2b3d1b8ee5da47c
Edwin <eyauyos@uni.pe>
2021-09-26 20:16:41 -0500

    Componente reubicado

--- src/pages/ServicesPage/ServicesPage.js
+++ src/pages/ServicesPage/ServicesPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Help from '../../components/ServicesPage/WeHelpYou/WeHelpYou'
+import Matrix from '../../components/ServicesPage/Matrix/Matrix'
 import ThirdSection from '../../components/ServicesPage/ThirdSection/ThirdSection'
 import ThirdData from '../../components/ServicesPage/ThirdSection/ThirdData.json'
 import FourthSection from '../../components/ServicesPage/FourthSection/FourthSection'
@@ -8,6 +9,7 @@ export default function ServicesPage() {
     return (
         <div>
             <Help/>
+            <Matrix/>
             <ThirdSection data={ThirdData.thirdSection}/>
             <FourthSection data={FourthData.fourthSection}/>
         </div>
