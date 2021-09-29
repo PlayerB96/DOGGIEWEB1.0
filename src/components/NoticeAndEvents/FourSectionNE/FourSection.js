a69ce55e15cf6236356bf94642a1b5c5e5a27700
Sergio Yepez <sergio.2435@hotmail.com>
2021-09-29 05:45:07 -0500

    Push en sección NoticeAndEvents:    -ThirdSection   80%    -FourSection     80%

---
+++ src/components/NoticeAndEvents/FourSectionNE/FourSection.js
@@ -0,0 +1,31 @@
+import react from "react";
+import Study1 from '../../../assets/imgs/NoticeAndEvents/FourSectionNE/Study1.png'
+import Study2 from '../../../assets/imgs/NoticeAndEvents/FourSectionNE/Study2.png'
+import Study3 from '../../../assets/imgs/NoticeAndEvents/FourSectionNE/Study3.png'
+import '../FourSectionNE/FourSection.scss'
+
+export default function FourSection(props){
+    return(
+
+        <div className="Container-Four">
+            <h1>{props.data.tittle}</h1>
+            <div className="Parent-Box">
+                <div className="Box">
+                    <img src={Study1}></img>
+                    <h2>{props.data.box1}</h2>
+                    <a>{props.data.info}</a>
+                </div>
+                <div className="Box">
+                    <img src={Study2}></img>
+                    <h2>{props.data.box2}</h2>
+                    <a>{props.data.info}</a>
+                </div>
+                <div className="Box">
+                    <img src={Study3}></img>
+                    <h2>{props.data.box3}</h2>
+                    <a>{props.data.info}</a>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/NoticeAndEvents/ThirdSectionNE/ThirdSection.js
@@ -0,0 +1,23 @@
+import react from "react";
+import GirlBooks from '../../../assets/imgs/NoticeAndEvents/ThirdSectionNE/GirlBooks.png'
+import '../ThirdSectionNE/ThirdSection.scss'
+export default function ThirdSection(props){
+    return(
+       <div className="Container">
+           <div className="Box-Image">
+                <img id="img" src={GirlBooks}></img>
+           </div>
+           <div className="Box-Text">
+                <p>{props.data.text1}</p>
+                <h1>{props.data.h1}</h1>
+                <p>{props.data.text2}</p>
+                <h2>{props.data.h2}</h2>
+                <h2>{props.data.h2}</h2>
+                <h2>{props.data.h2}</h2>
+                <h3>{props.data.h3}</h3>
+                <p>{props.data.text3}</p>
+                <a href="#">{props.data.btn}</a>
+           </div>
+       </div>
+    )
+}
