7b33a057939cf364476cb31567a6f0068a5fc8e6
Diego Bergna Aguilar <diego.bergna.a@uni.pe>
2021-09-29 11:48:26 -0500

    added resourcepage routes

--- src/components/ResourcePage/ResourceNav/ResourceNav.js
+++ src/components/ResourcePage/ResourceNav/ResourceNav.js
@@ -3,18 +3,28 @@ import './ResourceNav.scss';
 import returnArrow from '../../../assets/imgs/returnArrow.svg'
 import lens from '../../../assets/imgs/search.svg'
 
+import { useHistory } from 'react-router';
+
 export default function ResourceNav(props) {
 
+    const history = useHistory();
     return (
         <div className="resource-nav-container">
             <div className="res-nav-header">
-                <div className="return">
+                <div className="return" onClick={() => {
+                    history.push("/");
+                    window.scrollTo(0, 0);
+                }}>
                     <img className="returnArrow" src={returnArrow} alt="" />
                     <div className="return-txt">
                         {props.data.return}
                     </div>
                 </div>
-                <button className="b-button">{props.data.soliCoti}</button>
+                <button className="b-button" onClick={() => {
+                    history.push("/form");
+                    window.scrollTo(0, 0);
+                }}
+                >{props.data.soliCoti}</button>
             </div>
             <div className="tittle-b">
                 {props.data.title}
