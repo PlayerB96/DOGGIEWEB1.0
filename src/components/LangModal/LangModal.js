b7f763636d959c49019cafbfb2363b43a4517bac
Diego Bergna Aguilar <diego.bergna.a@uni.pe>
2021-09-29 16:50:41 -0500

    langModal (99%)

--- src/components/LangModal/LangModal.js
+++ src/components/LangModal/LangModal.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import blackLocation from '../../assets/imgs/LangModal/blackLocation.png'
+import yellowLocation from '../../assets/imgs/LangModal/yellowLocation.png'
 import logo from '../../assets/imgs/LangModal/logo.svg'
 import './LangModal.scss'
 
@@ -11,7 +13,108 @@ export default function LangModal(props) {
                     <button className="exitBtn" onClick={props.modalAction}>X</button>
                 </div>
                 <div className="bodyModal">
-                    <div>{props.data.title}</div>
+                    <div className="body-modal-title">
+                        {props.data.title}
+                    </div>
+                    <div className="continent-lan-title">
+                        {props.data.continents[0].name}
+                    </div>
+                    <div className="continent-lan-container">
+                        <div className="continent-lan">
+                            <img className="location" src={blackLocation} alt="" />
+                            <div className="lan-info">
+                                <div className="country">
+                                    {props.data.continents[0].countries[0].name}
+                                </div>
+                                <div className="lan">
+                                    {props.data.continents[0].countries[0].language}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="continent-lan">
+                            <img className="location" src={blackLocation} alt="" />
+                            <div className="lan-info">
+                                <div className="country">
+                                    {props.data.continents[0].countries[1].name}
+                                </div>
+                                <div className="lan">
+                                    {props.data.continents[0].countries[1].language}
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className="continent-lan">
+                            <img className="location" src={blackLocation} alt="" />
+                            <div className="lan-info">
+                                <div className="country">
+                                    {props.data.continents[0].countries[2].name}
+                                </div>
+                                <div className="lan">
+                                    {props.data.continents[0].countries[2].language}
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="continent-lan-title">
+                        {props.data.continents[1].name}
+                    </div>
+                    <div className="continent-lan-container">
+                        <div className="continent-lan">
+                            <img className="location" src={blackLocation} alt="" />
+                            <div className="lan-info">
+                                <div className="country">
+                                    {props.data.continents[1].countries[0].name}
+                                </div>
+                                <div className="lan">
+                                    {props.data.continents[1].countries[0].language}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="continent-lan-title">
+                        {props.data.continents[2].name}
+                    </div>
+                    <div className="continent-lan-container">
+                        <div className="continent-lan">
+                            <img className="location" src={blackLocation} alt="" />
+                            <div className="lan-info">
+                                <div className="country">
+                                    {props.data.continents[2].countries[0].name}
+                                </div>
+                                <div className="lan">
+                                    {props.data.continents[2].countries[0].language}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="continent-lan">
+                            <img className="location" src={blackLocation} alt="" />
+                            <div className="lan-info">
+                                <div className="country">
+                                    {props.data.continents[2].countries[1].name}
+                                </div>
+                                <div className="lan">
+                                    {props.data.continents[2].countries[1].language}
+                                </div>
+                            </div>
+
+
+                        </div>
+                        <div className="continent-lan">
+                            <img className="location" src={blackLocation} alt="" />
+                            <div className="lan-info">
+                                <div className="country">
+                                    {props.data.continents[2].countries[2].name}
+                                </div>
+                                <div className="lan">
+                                    {props.data.continents[2].countries[2].language}
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
