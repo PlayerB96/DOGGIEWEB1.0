473f397dd65621aa98a1323dd5b0e96900c6aa57
Edwin <eyauyos@uni.pe>
2021-09-29 14:15:31 -0500

    Agregado al flujo de N&E 3th y 4th componente

--- src/pages/NoticeAndEvents/NoticeAndEvents.js
+++ src/pages/NoticeAndEvents/NoticeAndEvents.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import FourSection from '../../components/NoticeAndEvents/FourSectionNE/FourSection'
+import ThirdSection from '../../components/NoticeAndEvents/ThirdSectionNE/ThirdSection'
+import NEData from './NewsData.json'
 export default function NoticeAndEvents() {
     return (
         <div>
-            
+            <ThirdSection data={NEData.thirdSection}></ThirdSection>
+            <FourSection data={NEData.fourthSection}></FourSection>
         </div>
     )
 }
